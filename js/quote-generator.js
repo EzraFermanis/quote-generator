e0d186367652723ea26973a97fac6b1a4b6e29d8
LizzieLaShark <lizzie.s.sullivan@gmail.com>
2016-02-26 14:55:23 +1300

    first part completed

--- js/quote-generator.js
+++ js/quote-generator.js
@@ -1,34 +1,42 @@
 
-var quotes = ["Cats are the best", "Cats are cray cray", "Cats are cool"]
+var quotes = ["A dog will flatter you but you have to flatter the cat.- George Mikes", "You can keep a dog; but it is the cat who keeps people, because cats find humans useful domestic animals.- George Mikes",
+"You own a dog but you feed a cat.- Jenny de Vries", "Some people say that cats are sneaky, evil, and cruel. True, and they have many other fine qualities as well. - Missy Dizick",
+"One is never sure, watching two cats washing each other, whether it's affection, the taste or a trial run for the jugular.- Helen Thomson",
+"Thousands of years ago, cats were worshipped as gods. Cats have never forgotten this.- Anonymous", "A cat sees no good reason why it should obey another animal, even if it does stand on two legs.- Sarah Thompson",
+"Cats are kindly masters, just so long as you remember your place.- Paul Gray", "One must love a cat on its own terms.- Paul Gray"];
+
 
 $(document).ready(function() {
 
-		
-var generateQuote = function(event) {
-		event.onmouseup" {
-		var randomNum = Math.floor((Math.random() * 11));
-		switch(randomNum) {
-			case 0:
-				console.log(quotes[0]);
-				break;
-			case 1:
-				console.log(quotes[1]);
-				break;
-			case 2:
-				console.log(quotes[2]);
-				break;
-		}
+	var gotButton = function(){document.getElementById("quoteButton");
+	};
 
+	var generateQuote = function() {
+			$("#quoteButton").mouseup( function () {
+				// console.log("event is working");
+				var randomNum = Math.floor((Math.random() * 3));
+				console.log(quotes[randomNum]);
+						
+					// else if (randomNum === 1) {
+					// 		console.log(quotes[1]);
+					// 	}
+					// else	{
+					// 		console.log(quotes[2]);
+					// 	}
+				
+		});
 	}
-}
 
-document.addEventListener("onmouseup", mouseup);
-generateQuote();
+	generateQuote();
 
-
+});
 
 
-};
+
+
+
+
+
 
 
 
